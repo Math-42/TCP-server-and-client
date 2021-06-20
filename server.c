262e64fa605ac69bb7f3ad31195310ea5d278eb3
Math-42 <matheus.v4200@gmail.com>
2021-06-20 16:40:21 -0300

    criando rotina de novas threads no servidor

--- server.c
+++ server.c
@@ -2,6 +2,13 @@
 #define MAX_MESSAGE_SIZE 80
 #define PORT 8081
 #define MAX_CONNECTIONS 10
+/**
+ * Rotina que cria uma thread para lidar com cada uma das conexões que o servidor recebe
+ * @param socketFileDescriptor file descriptor do próprio servidor
+ * @param serverAddr struct contendo os dados do servidor
+ */
+void threadCreationRoutine(int socketFileDescriptor, struct sockaddr_in serverAddr);
+
 void main() {
 
     int socketFileDescriptor;
@@ -29,4 +36,28 @@ void main() {
     printf("Escutando conexões\n");
 
     printf("Iniciando rotina principal do servidor\n");
+void threadCreationRoutine(int socketFileDescriptor, struct sockaddr_in serverAddr) {
+    //loop de funcionamento do servidor
+    while(1) {
+
+        int* connectionFileDescriptor = (int*)malloc(sizeof(int));
+
+        //Espera até uma conexão aparecer e aceita
+        if (!acceptConection(&socketFileDescriptor, &serverAddr, connectionFileDescriptor)) {
+            perror("Falha na conexão\n");
+            free(connectionFileDescriptor);
+            continue;
+        }
+        printf("Conexão estabelecida com sucesso\n");
+
+        int rcr;
+        pthread_t serverThread;
+
+        //cria uma nova thread que roda a rotina de servidor
+        if ( (rcr = pthread_create(&serverThread, NULL, serverRoutine, (void *)connectionFileDescriptor)) ){
+            printf("Criação da thread falhou: %d\n", rcr);
+        }
+    
+    }
+}
 }
