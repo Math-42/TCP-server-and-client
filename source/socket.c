7602cc30849b9430fe857bf2929f864a187b6fb9
Math-42 <matheus.v4200@gmail.com>
2021-06-20 16:31:20 -0300

    implementando função de criação de socket

--- source/socket.c
+++ source/socket.c
@@ -1,1 +1,16 @@
 #include "socket.h"
+
+int createSocket(int* socketFileDescriptor,struct sockaddr_in *socketAddr, unsigned short PORT, char* address) {
+
+	*socketFileDescriptor = socket(PF_INET, SOCK_STREAM, getprotobyname("tcp")->p_proto);//Dominio do socket, tipo do socket, protocolo
+
+	if(*socketFileDescriptor == -1) return 0;
+
+	bzero(socketAddr, sizeof(*socketAddr));//seta toda memória como 0
+
+    socketAddr->sin_family = AF_INET;  //Setando como IPv4
+    socketAddr->sin_addr.s_addr = (address == NULL)? htonl(INADDR_ANY): inet_addr(address); //Endereço especial que permite rodar sem saber o ip da maquina
+    socketAddr->sin_port = htons(PORT);//Seta a porta utilizando Network Byte Order AKA Big Endian
+
+	return 1;
+}
--- source/socket.h
+++ source/socket.h
@@ -11,5 +11,14 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+/**
+ * Cria um socket e seta suas configurações de endereço
+ * @param socketFileDescriptor file descriptor
+ * @param socketAddr struct do endereço do socket
+ * @param PORT porta que será utilizada para estabelecer a conexão
+ * @param address endereço da conexão, caso seja NULL usará o IP local
+ * @return retorna 0 caso ocorra algum erro e 1 caso de certo
+ */
+int createSocket(int* socketFileDescriptor,struct sockaddr_in *socketAddr, unsigned short PORT, char* address);
 
 #endif
