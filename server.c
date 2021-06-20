f8dc49946283d616e2262fe9e648cde9b83662ec
Math-42 <matheus.v4200@gmail.com>
2021-06-20 16:39:41 -0300

    fazendo bind e iniciando escuta do servidor

--- server.c
+++ server.c
@@ -8,9 +8,25 @@ void main() {
     struct sockaddr_in serverAddr;
 
     // Tenta criar o socket
-    if(!createSocket(&socketFileDescriptor, &serverAddr, 8080, NULL)) {
+    if(!createSocket(&socketFileDescriptor, &serverAddr, PORT, NULL)) {
         perror("Ocorreu um erro na criação do socket");
         return;
     }
     printf("Socket criado com sucesso\n");
 
+    // Tenta realziar o bind
+    if(!bindSocket(&socketFileDescriptor, &serverAddr)) {
+        perror("Criação do bind falhou");
+        return;
+    }
+    printf("Bind realizado com sucesso\n");
+
+    //Começa a ouvir a porta predefinida
+    if (!startListen(&socketFileDescriptor, MAX_CONNECTIONS)) {
+        printf("Falha no inicio da escuta\n");
+        return;
+    }
+    printf("Escutando conexões\n");
+
+    printf("Iniciando rotina principal do servidor\n");
+}
