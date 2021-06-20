91ed003e4452d17264ab1c6cf2f1e20b2fce40ac
Math-42 <matheus.v4200@gmail.com>
2021-06-20 16:42:24 -0300

    criando socket e conectando ao servidor

--- client.c
+++ client.c
@@ -1,5 +1,24 @@
 #include "source/socket.h"
 
 void main() {
+    int socketFileDescriptor;
+    struct sockaddr_in serverAddr;
 
+    // Tenta criar o socket
+    if (!createSocket(&socketFileDescriptor, &serverAddr, PORT, "127.0.0.1")) {
+        perror("Ocorreu um erro na criação do socket");
+        return;
+    }
+    printf("Socket criado\n");
+
+    // Tenta criar o socket
+    if (!connectToServer(&socketFileDescriptor, &serverAddr)) {
+        perror("Ocorreu um erro na conexão com o servidor");
+        return;
+    }
+    printf("Conexão estabelecida\n");
+
+    clientRoutine(socketFileDescriptor);
+
+    close(socketFileDescriptor);
 }
