8f5855bd89edb330404272ec02ec9e57f173e052
Math-42 <matheus.v4200@gmail.com>
2021-06-21 01:35:53 -0300

    iniciando criação da daemon

---
+++ source/daemon.h
@@ -0,0 +1,16 @@
+#ifndef DAEMON_H
+#define DAEMON_H
+
+#include <sys/syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <signal.h>
+
+
+
+#endif
