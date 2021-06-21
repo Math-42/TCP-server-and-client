3f365344650f6596a575164a698c7c38dbe35dab
Math-42 <matheus.v4200@gmail.com>
2021-06-21 13:51:59 -0300

    documentando daemon

--- source/daemon.h
+++ source/daemon.h
@@ -19,25 +19,30 @@ typedef struct daemonStruct{
 } daemonStruct;
 
 /**
- * Transforma o programa em uma daemon
+ * Rotina de inicialização da deamon, abre o log, cria o arquivo de log e define como os sinais serão tratados
+ * @param currDaemonInfos struct contendo as informações da daemon
+ * @param signalHandler função que lidará com os sinais recebidos pela daemon
+ * @param configHandler função que receberá as configurações lidas pela daemon
  */
 void initDaemon(daemonStruct* currDaemonInfos, void (* signalHandler)(), void (configHandler)(int *));
 
 /**
- * 
- * 
+ * Lê o arquivo de configurações, gera um array com os valores lidos e chama a função passada
+ * @param configurationFileName nome do arquivo onde as configurações estão salvas
+ * @param configHandler função que receberá as configurações lidas
  */
 void readConfigFile(char* configurationFileName, void (* configHandler)());
 
 /**
- * 
- * 
+ * Inicializa o arquivo de log
+ * @param currDaemonInfos struct contendo as informações da daemon
  */
 void setupLogFile(daemonStruct* currDaemonInfos);
 
 /**
- * 
- * 
+ * Rotina de finalização da daemon, fecha o arquivo de log e o log
+ * TODO: adicionar função de finalização
+ * @param currDaemonInfos struct contendo as informações da daemon
  */
 void finishDaemonRoutine(daemonStruct* currDaemonInfos);
 
